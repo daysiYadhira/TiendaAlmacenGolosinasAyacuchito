eda866661c3cc7e8395145d224772b0b9a7eb2e9
Daysi Pajuelo Tasayco <113059462+daysiYadhira@users.noreply.github.com>
2023-07-23 19:21:23 -0500

    Add files via upload

---
+++ header.php
@@ -0,0 +1,216 @@
+<?php
+if (strlen(session_id()) < 1)
+    session_start();
+?>
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Sistema de control de Almacen | TIENDA MINORISTA GOLOSINAS AYACUCHITO</title>
+        <!-- Tell the browser to be responsive to screen width -->
+        <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+        <!-- Bootstrap 3.3.7 -->
+        <link rel="stylesheet" href="../public/css/bootstrap.min.css">
+        <!-- Font Awesome -->
+
+        <link rel="stylesheet" href="../public/css/font-awesome.min.css">
+
+        <link rel="stylesheet" href="../public/css/AdminLTE.min.css">
+        <link rel="stylesheet" href="../public/css/_all-skins.min.css">
+        <!-- Morris chart --><!-- Daterange picker -->
+        <link rel="stylesheet" href="img/apple-touch-ico.png">
+        <link rel="stylesheet" href="img/favicon.ico">
+        <!-- DATATABLES-->
+        <link rel="stylesheet" href="../public/datatables/jquery.dataTables.min.css">
+        <link rel="stylesheet" href="../public/datatables/buttons.dataTables.min.css">
+        <link rel="stylesheet" href="../public/datatables/responsive.dataTables.min.css">
+        <link rel="stylesheet" href="../public/css/bootstrap-select.min.css">
+
+    </head>
+    <body class="hold-transition skin-blue sidebar-mini">
+        <div class="wrapper">
+
+            <header class="main-header">
+                <!-- Logo -->
+                <a href="escritorio.php" class="logo">
+                    <!-- mini logo for sidebar mini 50x50 pixels -->
+                    <span class="logo-mini"><img width="40px" src="../assets/img/logot.png" alt=""/>  </span>
+
+                    <!-- logo for regular state and mobile devices -->
+                    <span class="logo-lg"><img width="200px"   src="../public/images/logo2.png" alt=""/></span>
+
+                </a>
+                <!-- Header Navbar: style can be found in header.less -->
+                <nav class="navbar navbar-static-top">
+                    <!-- Sidebar toggle button-->
+                    <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
+                        <span class="sr-only"></span>
+                    </a>
+
+                    <div class="navbar-custom-menu">
+                        <ul class="nav navbar-nav">
+
+                            <li class="dropdown user user-menu">
+                            <MARQUEE SCROLLDELAY =180>								<!-- Date-JavaScript -->
+                                Per&uacute;, <script type="text/javascript">
+                                    var mydate = new Date()
+                                    var year = mydate.getYear()
+                                    if (year < 1000)
+                                        year += 1900
+                                    var day = mydate.getDay()
+                                    var month = mydate.getMonth()
+                                    var daym = mydate.getDate()
+                                    if (daym < 10)
+                                        daym = "0" + daym
+                                    var dayarray = new Array("Domingo", "Lunes", "Martes", "Mi&eacute;rcoles", "Jueves", "Viernes", "S&aacute;bado")
+                                    var montharray = new Array("Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre")
+                                    document.write("  " + dayarray[day] + " " + daym + " de " + montharray[month] + " del  " + year + " ")
+                                </script>
+                                <!-- //Date-JavaScript --> </MARQUEE>
+                            </li> 
+
+                            <li class="dropdown user user-menu">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                                    <img src="../files/usuarios/<?php echo $_SESSION['imagen']; ?>" class="user-image" alt="User Image">
+                                    <span class="hidden-xs"><?php echo $_SESSION['nombre']; ?></span>
+                                </a>
+                                <ul class="dropdown-menu">
+                                    <!-- User image -->
+                                    <li class="user-header">
+                                        <img src="../files/usuarios/<?php echo $_SESSION['imagen']; ?>" class="img-circle" alt="User Image"><a href="../ajax/usuario.php?op=salir" class="btn btn-default btn-flat">
+                                            <img src="../public/images/salir.png" alt=""/>  Salir</a>
+
+
+                                    </li>
+
+                                </ul>
+                            </li>
+                            <!-- Control Sidebar Toggle Button -->
+
+                        </ul>
+                    </div>
+                </nav>
+            </header>
+            <!-- Left side column. contains the logo and sidebar -->
+            <aside class="main-sidebar">
+                <!-- sidebar: style can be found in sidebar.less -->
+                <section class="sidebar">
+                    <!-- Sidebar user panel -->
+
+                    <!-- /.search form -->
+                    <!-- sidebar menu: : style can be found in sidebar.less -->
+                    <ul class="sidebar-menu" data-widget="tree">
+
+                        <br>
+                        <?php
+                        if ($_SESSION['escritorio'] == 1) {
+                            echo ' <li><a href="escritorio.php"><i class="fa  fa-dashboard (alias)"></i> <span>Escritorio</span></a>
+        </li>';
+                        }
+                        ?>
+                        <?php
+                        if ($_SESSION['almacen'] == 1) {
+                            echo ' <li class="treeview">
+          <a href="#">
+            <i class="fa fa-laptop"></i> <span>Almacén</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="articulo.php"><i class="fa fa-circle-o"></i> Productos</a></li>
+            <li><a href="categoria.php"><i bg-red class="fa fa-circle-o"></i> Categorías</a></li>
+          </ul>
+        </li>';
+                        }
+                        ?>
+                        <?php
+                        if ($_SESSION['compras'] == 1) {
+                            echo ' <li class="treeview">
+          <a href="#">
+            <i class="fa fa-th"></i> <span>Entrada</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="ingreso.php"><i class="fa fa-circle-o"></i> Ingresos</a></li>
+            <li><a href="proveedor.php"><i class="fa fa-circle-o"></i> Proveedores</a></li>
+          </ul>
+        </li>';
+                        }
+                        ?>
+
+                        <?php
+                        if ($_SESSION['ventas'] == 1) {
+                            echo '<li class="treeview">
+          <a href="#">
+            <i class="fa fa-shopping-cart"></i> <span>Salida</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="venta.php"><i class="fa fa-circle-o"></i> Despacho</a></li>
+            <li><a href="cliente.php"><i class="fa fa-circle-o"></i> Clientes</a></li>
+          </ul>
+        </li>';
+                        }
+                        ?>
+
+                        <?php
+                        if ($_SESSION['acceso'] == 1) {
+                            echo '  <li class="treeview">
+          <a href="#">
+            <i class="fa fa-folder"></i> <span>Acceso</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="usuario.php"><i class="fa fa-circle-o"></i> Usuarios</a></li>
+                  </ul>
+        </li>';
+                        }
+                        ?>  
+                        <?php
+                        if ($_SESSION['consultac'] == 1) {
+                            echo '     <li class="treeview">
+          <a href="#">
+            <i class="fa fa-bar-chart"></i> <span>Consulta Ingresos</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="comprasfecha.php"><i class="fa fa-circle-o"></i>Ingresos por fechas</a></li>
+          </ul>
+        </li>';
+                        }
+                        ?>  
+
+                        <?php
+                        if ($_SESSION['consultav'] == 1) {
+                            echo '<li class="treeview">
+          <a href="#">
+            <i class="fa fa-bar-chart"></i> <span>Consulta Salidas</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="ventasfechacliente.php"><i class="fa fa-circle-o"></i> Consulta Salidas</a></li>
+
+          </ul>
+        </li>';
+                        }
+                        ?>     
+
+
+
+                    </ul>
+                </section>
+                <!-- /.sidebar -->
+            </aside>
---
+++ re_contrase+¦a.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Login</title>
+    
+        <link href="css/fondocss.css" rel="stylesheet" type="text/css"/>
+</head>
+<body style="background-image:url(img/fondo.almacen.jpg)">
+    
+        <div class="login-caja"> 
+       <img src="img/Teca.PNG" alt="" width="310" height="60"  align ="left"/>
+      <img src="img/download.jpg" class="avatar" alt="Avatar Image">
+        
+            <form action="#"  method="POST" >
+          <br>
+        <br>  
+        <input type="text" name="txtusuario" placeholder="Ingrese usuario" required> 
+        
+        <input type="password" name="txtpassword" placeholder="Ingrese contraseña" required>
+                
+        <input type="text" name="txtcorreo" placeholder="Ingrese email" required>
+                <input type="submit" value="Recuperar cuenta">
+                 <a href="../index.php">&nbsp; -Ya tengo cuenta-</a>
+                 
+      </form>
+    </div>
+</html>
+
